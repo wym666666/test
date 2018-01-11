3a6417e6d58bd7040c88011bfcf91355c8c04235
666 <666@10.153.247.167>
2018-01-11 09:11:20 +0800

    11111111111111111111111111

---
+++ com.hadoop.myhadoop/src/main/java/com/hadoop/myhadoop/MaxTempeatureMapper.java
@@ -0,0 +1,37 @@
+package com.hadoop.myhadoop;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.mapreduce.Mapper.Context;
+
+public class MaxTempeatureMapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+
+	private static final int MISSING = 9999;
+	
+	String rlg = "rilegou";
+	
+//	@Override
+	public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+		String line = value.toString();
+		String year = line.substring(15,19);
+		int temp = Integer.parseInt(line);
+		if ("1949".equals(year)) {
+			context.write(new Text(year), new IntWritable(temp));
+		}
+	}
+
+	@Override
+	public void map(LongWritable arg0, Text arg1,
+			OutputCollector<Text, IntWritable> arg2, Reporter arg3)
+			throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+}
